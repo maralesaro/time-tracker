fe69b7bb6cf806337319d447a605194d1eac861c
maralesaro <51711430+maralesaro@users.noreply.github.com>
2019-06-17 08:49:42 -0500

    falla a proposito

--- core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
@@ -1,4 +1,4 @@
-package training.taylor.timetracker.core;
+ package training.taylor.timetracker.core;
 
 import static org.junit.Assert.*;
 
@@ -30,6 +30,6 @@ public class TrackerTest {
         entry.setRate(80.0f);
         entry.setTime(3);
         tracker.add(entry);
-        assertTrue(tracker.size() > 0);
+        assertTrue(tracker.size() < 0);
     }
 }
