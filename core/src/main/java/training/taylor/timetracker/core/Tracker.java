f97e9daa635f1e2bc155eb08c26d068ada254a23
maralesaro <51711430+maralesaro@users.noreply.github.com>
2019-06-17 16:24:23 -0500

    quitando issues in tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -25,20 +25,6 @@ public class Tracker {
     public int size() {
         return entries.size();
     }
-
-    public TimeEntry get(int index) {
-        try {
-
-        } catch (Exception e) {
-
-        }
-
-        boolean valid = false;
-
-        if (valid = true) {
-            // whatever
-        }
-
         return entries.get(index);
     }
 }
