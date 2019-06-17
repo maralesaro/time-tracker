f07491dc18b71caeaa6cf1eaaa4b4f899c62b922
maralesaro <51711430+maralesaro@users.noreply.github.com>
2019-06-17 16:29:53 -0500

    deshecho

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -20,11 +20,23 @@ public class Tracker {
             entries.remove(entry);
 
         entries.remove(entry);
-    }
-
+    }   
     public int size() {
         return entries.size();
     }
+    public TimeEntry get(int index) {
+        try {
+
+        } catch (Exception e) {
+
+        }
+
+        boolean valid = false;
+
+        if (valid = true) {
+            // whatever
+        }
+
         return entries.get(index);
     }
 }
