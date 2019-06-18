deb361a682b4a06399e1a10e743f176f4a68859d
maralesaro <51711430+maralesaro@users.noreply.github.com>
2019-06-18 09:05:00 -0500

    limpia issues

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -25,18 +25,6 @@ public class Tracker {
         return entries.size();
     }
     public TimeEntry get(int index) {
-        try {
-
-        } catch (Exception e) {
-
-        }
-
-        boolean valid = false;
-
-        if (valid = true) {
-            // whatever
-        }
-
         return entries.get(index);
     }
 }
